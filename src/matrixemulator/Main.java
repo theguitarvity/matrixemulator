5ef5cd956660846e85633d99d2db0d0ccbcede3a
Dimitrius Fernandes <pedroaugusto@jera.com.br>
2018-10-30 07:50:27 -0300

    modificando Main

--- src/matrixemulator/Main.java
+++ src/matrixemulator/Main.java
@@ -1,29 +1,46 @@
-package matrixemulator;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.ByteBuffer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String fileName = "out.bin";
-		try {
-			FileOutputStream fileOs = new FileOutputStream(fileName);
-			ObjectOutputStream os = new ObjectOutputStream(fileOs);
-			os.writeInt(2048);
-			os.writeDouble(3.1415);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("Escrita feita!");
+  private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
+  public static void main(String[] args) {
+
+    Path data = Paths.get(args[0]);
+
+    try {
+      byte[] bytes = Files.readAllBytes(data);
+
+      String teste = String.format("%8s", Integer.toBinaryString(bytes & 0xFF)).replace(' ', '0');
+
+      System.out.println(teste);
+
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+  }
+
+  public static void decode(List<Integer> instructions) {
+    for(int i = 0; i < instructions.size(); i++) {
+      System.out.println(instructions.get(i));
+    }
+  }
+
+  public static int[] convertToIntArray(byte[] input)
+	{
+	    int[] ret = new int[input.length];
+	    for (int i = 0; i < input.length; i++)
+	    {
+	        ret[i] = input[i];
+	    }
+	    return ret;
 	}
 
 }
