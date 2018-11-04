fd99983adb7932eaf80c36f445d01bb1a77befdf
Pedro <pee.dro_fs@hotmail.com>
2018-11-04 15:45:03 -0300

    names

--- src/matrixemulator/WriteBack.java
+++ src/matrixemulator/WriteBack.java
@@ -7,34 +7,31 @@ import matrixemulator.Run;
 
 public class WriteBack extends Run {
 
-	private final PipelineRegister mem_wb;
+	private final PipelineRegister mem_writeback;
 	private final RegisterFile registerFile;
 	
-	public WriteBack(
-		PipelineRegister mem_wb,
-		RegisterFile registerFile
-	) {
-		this.mem_wb = mem_wb;
+	public WriteBack(PipelineRegister mem_writeback, RegisterFile registerFile) {
+		this.mem_writeback = mem_writeback;
 		this.registerFile = registerFile;
 	}
 	
 	public boolean done() {
-		return mem_wb.getValue(RegisterName.HALT) == 1;
+		return mem_writeback.getValue(RegisterName.HALT) == 1;
 	}
 	
 	@Override
 	public void run() {
 		System.out.println("RUNNING WRITE BACK - - - - -");
-		System.out.println(mem_wb.getValue(RegisterName.REG_WRITE) == 1);
+		System.out.println(mem_writeback.getValue(RegisterName.REG_WRITE) == 1);
 		
-		if (mem_wb.getValue(RegisterName.REG_WRITE) == 1) {
-			long dest = mem_wb.getValue(RegisterName.REG_DST) == 1 ?
-				mem_wb.getValue(RegisterName.R_D) :
-				mem_wb.getValue(RegisterName.R_T);
+		if (mem_writeback.getValue(RegisterName.REG_WRITE) == 1) {
+			long dest = mem_writeback.getValue(RegisterName.REG_DST) == 1 ?
+				mem_writeback.getValue(RegisterName.R_D) :
+				mem_writeback.getValue(RegisterName.R_T);
 			
-			long value = mem_wb.getValue(RegisterName.MEM_TO_REG) == 1 ?
-				mem_wb.getValue(RegisterName.MEM_RESULT) :
-				mem_wb.getValue(RegisterName.ALU_RESULT);
+			long value = mem_writeback.getValue(RegisterName.MEM_TO_REG) == 1 ?
+				mem_writeback.getValue(RegisterName.MEM_RESULT) :
+				mem_writeback.getValue(RegisterName.ALU_RESULT);
 
 				System.out.println("DEST writeback - - - - -");
 				System.out.println(dest);
